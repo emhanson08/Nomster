ec0fc8db81f3193ac6e078b50f47f09e56c2921f
Elizabeth Hanson <emhanson08@gmail.com>
2017-08-23 23:00:12 +0000

    Completed Lesson 13

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,8 @@ class PlacesController < ApplicationController
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 1)
   end
+
+  def new
+    @place = place.new
+  end
 end
