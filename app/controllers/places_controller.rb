92f79f80e28917fad7c798d3e45ae61c4d5ec3bb
Elizabeth Hanson <emhanson08@gmail.com>
2017-08-24 18:32:39 +0000

    Completed Lesson 14_having trouble with pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,6 +4,15 @@ class PlacesController < ApplicationController
   end
 
   def new
-    @place = place.new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+    redirect_to root_path
   end
 end
